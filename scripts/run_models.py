146ac91c1f1d8c796c09905488dee0d1b8bd2fbb
Janne Mäyrä <janne.mayra@syke.fi>
2020-12-09 10:53:56 +0200

    fix geotiff opening

--- scripts/run_models.py
+++ scripts/run_models.py
@@ -12,26 +12,21 @@ from sklearn.ensemble import RandomForestRegressor
 
 import argparse
 
-def run_models(data_path:str, outdir:str):
+def run_models(data_path:str, outdir:str, log_y:bool=False):
     target_variables = ['v', 'g', 'h', 'd', 'v_ma', 'v_ku', 'v_lp']
 
     data_path = Path(data_path)
-
+    if not os.path.exists(outdir): os.makedirs(outdir)
     preprocessor = EnvecoPreprocessor(data_path/'AV.leaf.on.train.csv', 
                                       data_path/'AV.leaf.on.val.csv',
                                       data_path/'AV.leaf.on.test.csv')
     for t in target_variables:
         if not os.path.exists(f'{outdir}/{t}'): os.makedirs(f'{outdir}/{t}')
-        trainval_tb, test_tb = preprocessor.preprocess_lidar(target_col=t, path=data_path/'AV_las/', 
+        trainval_tb, test_tb = preprocessor.preprocess_lidar(target_col=t, 
+                                                             path=data_path/'AV_las/', 
                                                              min_h=1.5,
-                                                             height_features=True,
-                                                             point_features=True, 
-                                                             intensity_features=True, 
-                                                             height_quantiles=True,
-                                                             point_proportions=True, 
-                                                             canopy_densities=True,
                                                              normalize=True,
-                                                             log_y=False)
+                                                             log_y=log_y)
 
         dls = trainval_tb.dataloaders(bs=64, y_block=RegressionBlock())
 
@@ -48,7 +43,7 @@ def run_models(data_path:str, outdir:str):
         val_res.to_csv(f'{outdir}/{t}/ann_val.csv', float_format='%.4f', index=False)
         val_interp = RegressionInterpretation.from_ensemble(ensemble)
 
-        fig = val_interp.plot_results()
+        fig = val_interp.plot_results(log_y=log_y)
         for f in fig: f.get_figure().savefig(f'{outdir}/{t}/ann_val.png', dpi=300, bbox_inches='tight')
 
         test_dls = test_tb.dataloaders(y_block=RegressionBlock(), shuffle_train=False, drop_last=False)
@@ -56,16 +51,16 @@ def run_models(data_path:str, outdir:str):
         test_res.to_csv(f'{outdir}/{t}/ann_test.csv', float_format='%.4f', index=False)
         test_interp =  RegressionInterpretation.from_ensemble(ensemble, dl=test_dls[0])
         
-        fig = test_interp.plot_results()
+        fig = test_interp.plot_results(log_y=log_y)
         for f in fig: f.get_figure().savefig(f'{outdir}/{t}/ann_test.png', dpi=300, bbox_inches='tight')
 
 
         rf = RandomForestRegressor(n_estimators=500, max_features=0.5, min_samples_leaf=4, oob_score=True)
         rf.fit(trainval_tb.train.xs, trainval_tb.train.ys.values.ravel())
-        fig = plot_sklearn_regression(rf, trainval_tb.valid.xs, trainval_tb.valid.ys)
+        fig = plot_sklearn_regression(rf, trainval_tb.valid.xs, trainval_tb.valid.ys,log_y=log_y)
         for f in fig: f.get_figure().savefig(f'{outdir}/{t}/rf_val.png', dpi=300, bbox_inches='tight')
 
-        fig = plot_sklearn_regression(rf, test_tb.train.xs, test_tb.train.ys)
+        fig = plot_sklearn_regression(rf, test_tb.train.xs, test_tb.train.ys, log_y=log_y)
         for f in fig: f.get_figure().savefig(f'{outdir}/{t}/rf_test.png', dpi=300, bbox_inches='tight')
 
 
@@ -73,4 +68,4 @@ def run_models(data_path:str, outdir:str):
 
 if __name__ == '__main__':
     # Todo argparse
-    run_models('../../enveco_data/enveco/', 'results')
+    run_models('../../enveco_data/enveco/', 'results_log', True)
